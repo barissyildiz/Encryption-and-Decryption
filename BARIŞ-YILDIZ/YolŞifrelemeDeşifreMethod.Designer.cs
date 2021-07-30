68c57fb1f27aeae57feaa96f45ac9ac7f9828d91
barissyildiz <81921189+barissyildiz@users.noreply.github.com>
2021-07-30 13:48:08 +0300

    Encryption and Decryption

---
+++ BARIŞ-YILDIZ/YolŞifrelemeDeşifreMethod.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace BARIŞ_YILDIZ
+{
+    partial class YolŞifrelemeDeşifreMethod
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_yöntem2_input_sentence = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_ready_output_text2 = new System.Windows.Forms.Label();
+            this.first_output_sentence = new System.Windows.Forms.Label();
+            this.label_ready_output_text1 = new System.Windows.Forms.Label();
+            this.şifreleme_deşifre_yöntem2_output_label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_yöntem2_table_measurement = new System.Windows.Forms.ComboBox();
+            this.comboBox_yöntem2_şifreleme_deşifre = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox_dilseçenegi = new System.Windows.Forms.ComboBox();
+            this.button_run_information_application = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_rules_information = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LÜTFEN ŞİFRELEME/DEŞİFRE YAPILACAK VERİYİ GİRİNİZ";
+            // 
+            // textBox_yöntem2_input_sentence
+            // 
+            this.textBox_yöntem2_input_sentence.Location = new System.Drawing.Point(29, 69);
+            this.textBox_yöntem2_input_sentence.Multiline = true;
+            this.textBox_yöntem2_input_sentence.Name = "textBox_yöntem2_input_sentence";
+            this.textBox_yöntem2_input_sentence.Size = new System.Drawing.Size(301, 86);
+            this.textBox_yöntem2_input_sentence.TabIndex = 1;
+            this.textBox_yöntem2_input_sentence.TextChanged += new System.EventHandler(this.textBox_yöntem2_input_sentence_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "METNİ İŞLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_yöntem2_run_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label_ready_output_text2);
+            this.panel1.Controls.Add(this.first_output_sentence);
+            this.panel1.Controls.Add(this.label_ready_output_text1);
+            this.panel1.Controls.Add(this.şifreleme_deşifre_yöntem2_output_label);
+            this.panel1.Location = new System.Drawing.Point(29, 244);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 283);
+            this.panel1.TabIndex = 3;
+            // 
+            // label_ready_output_text2
+            // 
+            this.label_ready_output_text2.AutoSize = true;
+            this.label_ready_output_text2.Location = new System.Drawing.Point(3, 152);
+            this.label_ready_output_text2.Name = "label_ready_output_text2";
+            this.label_ready_output_text2.Size = new System.Drawing.Size(233, 13);
+            this.label_ready_output_text2.TabIndex = 3;
+            this.label_ready_output_text2.Text = "E3 Matrisindem Filtreleme Sonucu Oluşan Matris";
+            // 
+            // first_output_sentence
+            // 
+            this.first_output_sentence.AutoSize = true;
+            this.first_output_sentence.Location = new System.Drawing.Point(3, 39);
+            this.first_output_sentence.Name = "first_output_sentence";
+            this.first_output_sentence.Size = new System.Drawing.Size(35, 13);
+            this.first_output_sentence.TabIndex = 2;
+            this.first_output_sentence.Text = "label5";
+            this.first_output_sentence.SizeChanged += new System.EventHandler(this.first_output_sentence_SizeChanged);
+            // 
+            // label_ready_output_text1
+            // 
+            this.label_ready_output_text1.AutoSize = true;
+            this.label_ready_output_text1.Location = new System.Drawing.Point(3, 14);
+            this.label_ready_output_text1.Name = "label_ready_output_text1";
+            this.label_ready_output_text1.Size = new System.Drawing.Size(231, 13);
+            this.label_ready_output_text1.TabIndex = 1;
+            this.label_ready_output_text1.Text = "A4 Matrisinden Filtreleme Sonucu Oluşan Matris";
+            // 
+            // şifreleme_deşifre_yöntem2_output_label
+            // 
+            this.şifreleme_deşifre_yöntem2_output_label.AutoSize = true;
+            this.şifreleme_deşifre_yöntem2_output_label.Location = new System.Drawing.Point(3, 182);
+            this.şifreleme_deşifre_yöntem2_output_label.Name = "şifreleme_deşifre_yöntem2_output_label";
+            this.şifreleme_deşifre_yöntem2_output_label.Size = new System.Drawing.Size(35, 13);
+            this.şifreleme_deşifre_yöntem2_output_label.TabIndex = 0;
+            this.şifreleme_deşifre_yöntem2_output_label.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(634, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ŞİFRELEMEDE OLUŞTURULABİLECEK\r\n   OLASI TABLO BOYUTUNU SEÇİNİZ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(662, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ŞİFRELEME/DEŞİFRE";
+            // 
+            // comboBox_yöntem2_table_measurement
+            // 
+            this.comboBox_yöntem2_table_measurement.FormattingEnabled = true;
+            this.comboBox_yöntem2_table_measurement.Location = new System.Drawing.Point(862, 150);
+            this.comboBox_yöntem2_table_measurement.Name = "comboBox_yöntem2_table_measurement";
+            this.comboBox_yöntem2_table_measurement.Size = new System.Drawing.Size(152, 21);
+            this.comboBox_yöntem2_table_measurement.TabIndex = 6;
+            this.comboBox_yöntem2_table_measurement.SelectedIndexChanged += new System.EventHandler(this.comboBox_yöntem2_table_measurement_SelectedIndexChanged);
+            // 
+            // comboBox_yöntem2_şifreleme_deşifre
+            // 
+            this.comboBox_yöntem2_şifreleme_deşifre.FormattingEnabled = true;
+            this.comboBox_yöntem2_şifreleme_deşifre.Location = new System.Drawing.Point(862, 255);
+            this.comboBox_yöntem2_şifreleme_deşifre.Name = "comboBox_yöntem2_şifreleme_deşifre";
+            this.comboBox_yöntem2_şifreleme_deşifre.Size = new System.Drawing.Size(152, 21);
+            this.comboBox_yöntem2_şifreleme_deşifre.TabIndex = 7;
+            this.comboBox_yöntem2_şifreleme_deşifre.SelectedIndexChanged += new System.EventHandler(this.comboBox_yöntem2_şifreleme_deşifre_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 43);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "ANA MENÜYE DÖN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_ana_menüye_dön2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(862, 338);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 43);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "TEMİZLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(668, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "DİL SEÇENEĞİ";
+            // 
+            // comboBox_dilseçenegi
+            // 
+            this.comboBox_dilseçenegi.FormattingEnabled = true;
+            this.comboBox_dilseçenegi.Location = new System.Drawing.Point(862, 207);
+            this.comboBox_dilseçenegi.Name = "comboBox_dilseçenegi";
+            this.comboBox_dilseçenegi.Size = new System.Drawing.Size(152, 21);
+            this.comboBox_dilseçenegi.TabIndex = 11;
+            this.comboBox_dilseçenegi.SelectedIndexChanged += new System.EventHandler(this.comboBox_dilseçenegi_SelectedIndexChanged);
+            // 
+            // button_run_information_application
+            // 
+            this.button_run_information_application.Location = new System.Drawing.Point(637, 69);
+            this.button_run_information_application.Name = "button_run_information_application";
+            this.button_run_information_application.Size = new System.Drawing.Size(376, 30);
+            this.button_run_information_application.TabIndex = 12;
+            this.button_run_information_application.Text = "METHODUN ÇALIŞMA ŞEKLİ";
+            this.button_run_information_application.UseVisualStyleBackColor = true;
+            this.button_run_information_application.Click += new System.EventHandler(this.button_run_information_application_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ŞİFRELEME/DEŞİFRE SONRASI VERİ";
+            // 
+            // button_rules_information
+            // 
+            this.button_rules_information.Location = new System.Drawing.Point(637, 22);
+            this.button_rules_information.Name = "button_rules_information";
+            this.button_rules_information.Size = new System.Drawing.Size(377, 33);
+            this.button_rules_information.TabIndex = 14;
+            this.button_rules_information.Text = "METHODUN DOĞRU ÇALIŞMASI İÇİN UYULMASI GEREKENLER";
+            this.button_rules_information.UseVisualStyleBackColor = true;
+            this.button_rules_information.Click += new System.EventHandler(this.button_rules_information_Click);
+            // 
+            // YolŞifrelemeDeşifreMethod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 611);
+            this.Controls.Add(this.button_rules_information);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button_run_information_application);
+            this.Controls.Add(this.comboBox_dilseçenegi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox_yöntem2_şifreleme_deşifre);
+            this.Controls.Add(this.comboBox_yöntem2_table_measurement);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_yöntem2_input_sentence);
+            this.Controls.Add(this.label1);
+            this.Name = "YolŞifrelemeDeşifreMethod";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YolŞifrelemeDeşifreMethod";
+            this.Load += new System.EventHandler(this.YolŞifrelemeDeşifreMethod_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_yöntem2_input_sentence;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label şifreleme_deşifre_yöntem2_output_label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_yöntem2_table_measurement;
+        private System.Windows.Forms.ComboBox comboBox_yöntem2_şifreleme_deşifre;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label_ready_output_text2;
+        private System.Windows.Forms.Label first_output_sentence;
+        private System.Windows.Forms.Label label_ready_output_text1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox_dilseçenegi;
+        private System.Windows.Forms.Button button_run_information_application;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_rules_information;
+    }
+}
