ae04f3d28895be9ad35415ab1b6a1b6c981d7375
barissyildiz <81921189+barissyildiz@users.noreply.github.com>
2021-07-30 13:19:32 +0300

    Add files via upload

---
+++ BARIŞ-YILDIZ/BARIŞ-YILDIZ/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace BARIŞ_YILDIZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DECİMATİON METHOD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(445, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "YOL ŞİFRELEME/DEŞİFRE METHOD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button_exit
+            // 
+            this.button_exit.Location = new System.Drawing.Point(315, 252);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(153, 52);
+            this.button_exit.TabIndex = 2;
+            this.button_exit.Text = "EXİT";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 490);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_exit;
+    }
+}
+
